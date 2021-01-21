5e26906541f3bbe90d13958b83d71e53ed23dda5
Alex <alex89.conti@gmail.com>
2021-01-21 23:39:58 +0100

    added instructions

--- test.py
+++ test.py
@@ -2,4 +2,9 @@ from pi74HC595 import pi74HC595
 import RPi.GPIO as gpio
 
 gpio.setmode(gpio.BOARD)
-shift_register = pi74HC595()
+shift_register = pi74HC595(11,13,15)
+
+if __name__ == "__main__":
+    while True:
+        shift_register.set_by_list([0, 1, 0, 1, 0, 1, 0, 1])
+        shift_register.set_by_list([1, 0, 1, 0, 1, 0, 1, 0])
