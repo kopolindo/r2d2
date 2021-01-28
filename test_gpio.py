c3cecde05b6f8c8367b77808b570764b3ef6799c
Alex <alex89.conti@gmail.com>
2021-01-28 08:26:52 +0000

    worked on test_gpio.py added utilities to replace pi74hc595 library that doesn't fit my needs 	tick: raising edge for clock pin (SHCP) 	latch: raising edge for latch pin (STCP) 	push_bit: alias for gpio.output(DS pin,value) 	set_list(l): given a list of bit (1/0) values it pushes those values to the shift register 	random_color_list(color_num): creates a list of random colors (constraint: two anodes with different values) In the main file: 	added possibility to import an external file with defined colors within (eg: rrrg) one for line 	added a while True loop to set random colors TODO: 	make the import a function 	daisy chain

--- test_gpio.py
+++ test_gpio.py
@@ -1,17 +1,6 @@
 # test alex
 import RPi.GPIO as gpio
-import signal,sys,time
-
-class LED(object):
-    def __init__(self, pin1: int, pin2: int):
-        self.pin1=pin1 # pin anodo sx
-        self.pin2=pin2 # pin anodo dx
-
-# define all LEDs
-LED1=LED(0,1)
-LED2=LED(2,3)
-LED3=LED(4,5)
-LED4=LED(6,7)
+import signal,sys,time,random
 
 # define signal handler to gracefully exit
 def signal_handler(sig, frame):
@@ -19,6 +8,8 @@ def signal_handler(sig, frame):
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
+# define ANODE number (my LEDs have two anodes and one common cathode)
+ANODE_NUM=2
 # set gpio mode and 74HC595 PINs
 gpio.setmode(gpio.BOARD)
 DS=11
@@ -29,16 +20,18 @@ gpio.setup(DS,gpio.OUT)
 gpio.setup(SH,gpio.OUT)
 gpio.setup(ST,gpio.OUT)
 
+# define time to sleep
+pause = 0.003
 def tick():
-    time.sleep(0.1)
+    time.sleep(pause)
     gpio.output(SH,1)
-    time.sleep(0.1)
+    time.sleep(pause)
     gpio.output(SH,0)
 
 def latch():
-    time.sleep(0.1)
+    time.sleep(pause)
     gpio.output(ST,1)
-    time.sleep(0.1)
+    time.sleep(pause)
     gpio.output(ST,0)
 
 def push_bit(bit):
@@ -50,66 +43,30 @@ def set_list(l):
         tick()
     latch()
 
-def LED_color(LED,color):
-    if color == "RED":
-        pin = LED.pin1
-    elif color ==  "GREEN":
-        pin = LED.pin2
-    useful_clocks = pin + 1
-    padding_clocks = 8 - useful_clocks
-    print("I want {} HIGH, so I need {} clocks".format(pin,useful_clocks))
-    for p in range(padding_clocks):
-        gpio.output(DS,0)
-        tick()
-    for i in range(useful_clocks):
-        if i == 0:
-            gpio.output(DS,1)
-        else:
-            gpio.output(DS,0)
-        tick()
+def random_color_list(color_num):
+    out = []
+    for i in range(color_num):
+        first=random.randint(0,1)
+        out.append(first)
+        out.append(abs(1-first))
+    return out
 
 if __name__ == "__main__":
-    # LED_color(LED4,"RED")
-    # latch()
-    #          R   R  OFF  G
-    set_list([1,0,1,0,0,0,0,1])
-    #          G   G  OFF  R
-    set_list([0,1,0,1,0,0,1,0])
-    #         OFF OFF  G  OFF
-    set_list([0,0,0,0,0,1,0,0])
-    #         OFF OFF  R  OFF
-    set_list([0,0,0,0,1,0,0,0])
+    color_list = []
+    with open(sys.argv[1]) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    for line in content:
+        colors = []
+        for c in line:
+            if c == "r" or c == "R":
+                colors += [1,0]
+            elif c == "g" or c == "G":
+                colors += [0,1]
+        color_list += [colors]
+    while True:
+        set_list(random_color_list(4))
+    # for i in range(0,len(color_list)):
+    #     #print("Line {} is bit list {}".format(content[i],color_list[i]))
+    #     set_list(color_list[i])
     gpio.cleanup()
-    """while 1:
-        for y in range(8):
-            val = 0
-            if (y % 2) == 0:
-                val = 1
-            # value to push and shift
-            gpio.output(DS,val)
-            time.sleep(0.1)
-            # CLOCK: shift val (1-0)
-            clock()
-            # clear DATA pin
-            gpio.output(DS,0)
-            # LATCH: output shifted DATA
-            latch()
-
-        for y in range(8):
-            val = 1
-            if (y % 2) == 0:
-                val = 0
-            # value to push and make shift
-            gpio.output(DS,val)
-            time.sleep(0.1)
-            # CLOCK: shift val (1-0)
-            gpio.output(SH,1)
-            time.sleep(0.1)
-            gpio.output(SH,0)
-            # clear DATA pin
-            gpio.output(DS,0)
-            # LATCH: output shifted DATA
-            gpio.output(ST,1)
-            time.sleep(0.1)
-            gpio.output(ST,0)
-        """
